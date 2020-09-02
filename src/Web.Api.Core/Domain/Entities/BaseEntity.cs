a66913a698874c758b9a49582731ab3ab3413e36
Tran Dinh Trung <tdtrung17693@localhost.localdomain>
2020-09-02 17:32:38 +0700

    Comment out navigation attributes

--- src/Web.Api.Core/Domain/Entities/BaseEntity.cs
+++ src/Web.Api.Core/Domain/Entities/BaseEntity.cs
@@ -34,8 +34,8 @@ namespace Web.Api.Core.Domain.Entities
 
         public bool? Status { get; set; }
         public bool? IsDeleted { get; set; }
-        public virtual User CreatedByNavigation { get; set; }
-        public virtual User DeletedByNavigation { get; set; }
-        public virtual User UpdatedByNavigation { get; set; }
+        // public virtual User CreatedByNavigation { get; set; }
+        // public virtual User DeletedByNavigation { get; set; }
+        // public virtual User UpdatedByNavigation { get; set; }
     }
 }
--- src/Web.Api.Core/Domain/Entities/User.cs
+++ src/Web.Api.Core/Domain/Entities/User.cs
@@ -24,18 +24,18 @@ namespace Web.Api.Core.Domain.Entities
 
         public virtual Role Role { get; set; }
         public virtual Account Account { get; set; }
-        public virtual ICollection<Comment> CommentCreatedByNavigation { get; set; }
-        public virtual ICollection<Comment> CommentDeletedByNavigation { get; set; }
-        public virtual ICollection<Comment> CommentUpdatedByNavigation { get; set; }
-        public virtual ICollection<Customer> CustomerCreatedByNavigation { get; set; }
-        public virtual ICollection<Customer> CustomerUpdatedByNavigation { get; set; }
-        public virtual ICollection<User> InverseCreatedByNavigation { get; set; }
-        public virtual ICollection<User> InverseDeletedByNavigation { get; set; }
-        public virtual ICollection<User> InverseUpdatedByNavigation { get; set; }
-        public virtual ICollection<Request> RequestApprovedByNavigation { get; set; }
-        public virtual ICollection<Request> RequestCreatedByNavigation { get; set; }
-        public virtual ICollection<Request> RequestDeletedByNavigation { get; set; }
-        public virtual ICollection<Request> RequestUpdatedByNavigation { get; set; }
+        // public virtual ICollection<Comment> CommentCreatedByNavigation { get; set; }
+        // public virtual ICollection<Comment> CommentDeletedByNavigation { get; set; }
+        // public virtual ICollection<Comment> CommentUpdatedByNavigation { get; set; }
+        // public virtual ICollection<Customer> CustomerCreatedByNavigation { get; set; }
+        // public virtual ICollection<Customer> CustomerUpdatedByNavigation { get; set; }
+        // public virtual ICollection<User> InverseCreatedByNavigation { get; set; }
+        // public virtual ICollection<User> InverseDeletedByNavigation { get; set; }
+        // public virtual ICollection<User> InverseUpdatedByNavigation { get; set; }
+        // public virtual ICollection<Request> RequestApprovedByNavigation { get; set; }
+        // public virtual ICollection<Request> RequestCreatedByNavigation { get; set; }
+        // public virtual ICollection<Request> RequestDeletedByNavigation { get; set; }
+        // public virtual ICollection<Request> RequestUpdatedByNavigation { get; set; }
         public virtual ICollection<UserFileInstance> UserFileInstance { get; set; }
         public virtual ICollection<UserLog> UserLog { get; set; }
     }
