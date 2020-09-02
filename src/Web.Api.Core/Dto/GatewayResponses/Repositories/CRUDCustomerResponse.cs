2fb80ff0da14bf6027ed5c05c4275997cb20544f
Nguyễn Trần Phương Khoa <phuongkhoa95ntp@gmail.com>
2020-09-02 22:38:42 +0700

    Small fix

--- src/Web.Api.Core/Dto/GatewayResponses/Repositories/CRUDCustomerResponse.cs
+++ src/Web.Api.Core/Dto/GatewayResponses/Repositories/CRUDCustomerResponse.cs
@@ -6,8 +6,8 @@ namespace Web.Api.Core.Dto.GatewayResponses.Repositories
 {
     public sealed class CRUDCustomerResponse : BaseGatewayResponse
     {
-        public Guid Id { get; }
-        public CRUDCustomerResponse(Guid id, bool success = false, IEnumerable<Error> errors = null) : base(success, errors)
+        public Guid? Id { get; }
+        public CRUDCustomerResponse(Guid? id, bool success = false, IEnumerable<Error> errors = null) : base(success, errors)
         {
             Id = id;
         }
