b9dc0e9e28549231e2076d9652f8e27e5da65653
Trần Đình Trung <trandinhtrung176@gmail.com>
2020-09-17 16:57:36 +0700

    add more migrations

--- src/Web.Api.Infrastructure/Migrations/20200917090227_sp-GetServerCount.cs
+++ src/Web.Api.Infrastructure/Migrations/20200917090227_sp-GetServerCount.cs
@@ -7,11 +7,21 @@ namespace Web.Api.Infrastructure.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-CREATE OR ALTER PROC GetServerCount
+CREATE OR ALTER PROC GetServerCount @FilterBy NVARCHAR(MAX)
 AS
 BEGIN
+    DECLARE @FilterStr NVARCHAR(MAX) = '%' + @FilterBy + '%';
 	SELECT Count([Server].Id) AS ServerCount
-	FROM [Server]
+	FROM dbo.[Server] LEFT OUTER JOIN
+		(
+	    SELECT dbo.CustomerServer.ServerId,dbo.Customer.Name
+	    FROM dbo.Customer JOIN dbo.CustomerServer ON [Customer].Id = [CustomerServer].CustomerId
+		) AS CS ON [Server].Id = CS.ServerId
+		WHERE (
+			[Server].[Name] LIKE @FilterStr OR
+			[CS].[Name] LIKE @FilterStr OR
+			[Server].IpAddress  LIKE @FilterStr
+		)
 END");
         }
 
---
+++ src/Web.Api.Infrastructure/Migrations/20200917095318_sp-CreateHistoryLog.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Api.Infrastructure.Migrations
+{
+    public partial class spCreateHistoryLog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR ALTER PROC CreateHistoryLog (
+    @RequestId UNIQUEIDENTIFIER,
+    @UpdatedField NVARCHAR(150),
+    @UpdatedState NVARCHAR(150),
+    @PreviousState NVARCHAR(150),
+    @Message NVARCHAR(150),
+    @CreatedBy NVARCHAR(150)
+)
+AS
+BEGIN
+    INSERT INTO dbo.[HistoryLog](Id, RequestId, CreatedAt, CreatedBy, UpdatedField, UpdatedState, PreviousState, [Message])
+    VALUES (NEWID(), @RequestId, GETUTCDATE(), @CreatedBy, @UpdatedField, @UpdatedState, @PreviousState, @Message)
+END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop proc CreateHistoryLog");
+        }
+    }
+}
